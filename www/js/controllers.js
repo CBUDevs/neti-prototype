50b59197202e43b8011eaf051aed388d2feead22
CalebSolorio <c.solorio@att.net>
2016-01-24 11:18:01 -0800

    First part of styling. Not complete, go to last version for functional product

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,11 @@
 angular.module('app.controllers', [])
   
-.controller('menuCtrl', function($scope, $timeout, $interval, Item) {
+.controller('menuCtrl', function($scope, $timeout, $interval, $sce, $stateParams, Item) {
+    $scope.title = "National Emerging Threats Initiative";
+    $scope.showSearch = false;
+    $scope.q = {}
+    $scope.q.name='';
+    
     $timeout(callAtTimeout, 300);
     function callAtTimeout(){
         $scope.items = Item.all();
@@ -8,6 +13,26 @@ angular.module('app.controllers', [])
     $interval(update, 1000);
     function update(){
         $scope.items = Item.all();
+    }
+    
+     $scope.item = Item.get($stateParams.itemUid);
+    $scope.setItem = function(index) {
+        $scope.item =  Item.get(index);
+    }
+    
+    $scope.setContent = function(index) {
+        $scope.content = $sce.trustAsHtml($scope.item.content[index]);
+    }
+    
+    $scope.setTitle = function(title, num) {
+        if (num) {
+            $scope.title = title;
+        } else {
+            $scope.title = "National Emerging Threats Initiative";
+        }
+    }
+    $scope.debug = function() {
+        console.log('click');
     }
 })
 
